757b375574f9d5bf87790e624c77c7840305c587
DanielDowling999 <ddow731@aucklanduni.ac.nz>
2024-06-13 15:51:21 +1200

    Added additional purchasable option, a farm, and created a new function to handle all point updates. Will likely be simplified further to only require one call, instead of one for each type.

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
 points = 0;
 clickers = 0;
-clickerCost = 10
-
+clickerCost = 10;
+clickerValue = 1
+farmCost = 150;
+farms = 0;
+farmValue = 10;
 function checkPoints(){
-    
     unlockExtraClick();
 };
 
+function pointUpdate(value, number){
+    points = points+(value*number);
+    document.getElementById("points").innerHTML = points;
+    checkPoints();
+    uiUpdate();
+}
 function pointClick(number){
     points = points+number;
     document.getElementById("points").innerHTML = points;
     checkPoints();
+    uiUpdate();
 };
 
 function unlockExtraClick(){
@@ -18,8 +27,26 @@ function unlockExtraClick(){
         document.getElementById("buyClicker").style.display ="block";
     else
         document.getElementById("buyClicker").style.display = "none";
-    
+    if (points >= farmCost)
+        document.getElementById("buyFarm").style.display = "block";
+    else
+        document.getElementById("buyFarm").style.display = "none";
 };
+
+function uiUpdate(){
+    if (clickers == 0 && points < clickerCost){
+        document.getElementById("clickerDiv").style.display = "none";
+    }
+    else{
+        document.getElementById("clickerDiv").style.display = "block";
+    }
+    if (farms == 0 && points < farmCost){
+        document.getElementById("farmDiv").style.display = "none";
+    }
+    else{
+        document.getElementById("farmDiv").style.display = "block";
+    }
+}
 
 function buyClick(){
     points = points-clickerCost;
@@ -32,7 +59,18 @@ function buyClick(){
     checkPoints();
 };
 
-window.setInterval(function(){
-    pointClick(clickers);
+function buyFarm(){
+    points = points-farmCost;
+    document.getElementById("points").innerHTML = points;
+    farms = farms+1;
+    document.getElementById("farms").innerHTML = farms;
+    farmCost = farmCost+100;
+    document.getElementById("farmCost").innerHTML = farmCost;
     checkPoints();
+}
+
+window.setInterval(function(){
+    pointUpdate(clickerValue, clickers);
+    pointUpdate(farmValue, farms);
+
 }, 1000);
